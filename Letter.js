4b4a9ebfd0f9d446cc3536b4cbb8d1fa5a930dde
RawKitMan <cjvaughn@rocketmail.com>
2019-01-14 10:47:00 -0700

    Added new display text and completed debugging. Project complete.

--- Letter.js
+++ Letter.js
@@ -1,7 +1,12 @@
+//Does not depend on any other files
+
+//Letter constructor that stores the letter for a word being guessed (in other files), has a value to determine if the letter
+//was guessed or not, and then displays either an underscore (if guessed === false) or the letter (if guessed === true);
 let Letter = function(letter){
     this.letter = letter;
     this.guessed = false;
     
+    //method checks the 'guessed' property and returns either an _ or letter based on the 'guessed' value
     this.displayLetter = function(){
         if(this.guessed){
             return this.letter + " ";
@@ -11,13 +16,14 @@ let Letter = function(letter){
         };
     };
 
+    //if the guessed letter matches the 'letter' value, then we change the 'guessed' value of the Letter object to 'true'.
     this.checkGuess = function(guess){
         if(guess === this.letter){
             this.guessed = true;
         }
-        this.guessed;
+        
     };
 };
 
-
+//Exporting the constructor to the Word.js file.
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -1,6 +1,11 @@
+
+//This file requires access to the Letter.js file
 let Letter = require("./Letter.js");
 
+//Our word constructor
 let Word = function(word){
+    
+    //Each Word constructor creates an array of Letter objects for each letter in the word.
     this.lettersArr = [];
     
     for(let i=0; i < word.length; i++){
@@ -12,10 +17,11 @@ let Word = function(word){
         };
     };
 
+    //Method that utilizes the displayLetter() method in each Letter object in the Word object and returns
+    //what the user will see in the game based on which letters are guessed correctly in the hidden word.
     this.createString = function(){
         let wordString = "";
 
-        //Needs to use the display letter function. Add this in tomorrow.
         this.lettersArr.forEach(function(element){
            if(element === " "){
                wordString += "  ";
@@ -27,14 +33,18 @@ let Word = function(word){
         return wordString;
     }
 
+
+    //When a letter is guessed, the Word object checks each Letter object in its array and changes the guessed value to true
+    //if the letter is correct.
     this.checkGuessWord = function(letterGuess){
         this.lettersArr.forEach(function(element){
             
-            if(element.letter !== " "){
+            if(element.letter !== undefined){
                 element.checkGuess(letterGuess);
             }
         });
     };
 };
 
+//Will be exporting the Word object to index.js
 module.exports = Word;
